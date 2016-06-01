2ce607afae09989caef0678772d2aec59b15637a
iPlowPlow <plowplow2011@hotmail.fr>
2016-06-01 17:08:41 +0200

    update jon

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ api.get('/', function(req,res,next){
 	res.send("hello word");
 });
 
+
+var port = process.env.port || DEFAULT_PORT;
 console.log('server started on port 3000');
 api.listen(3000);
 
