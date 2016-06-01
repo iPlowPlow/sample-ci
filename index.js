022d212cd0524734b317d45fa6963896e05bff52
iPlowPlow <plowplow2011@hotmail.fr>
2016-06-01 17:26:25 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var api = express();
-
+var port = process.env.port || DEFAULT_PORT;
 
 api.get('/', function(req,res,next){
 	res.send("hello word");
 });
 
+api.get('/contacts/:name', function(req,res,next){
+	res.send();
+})
 
-var port = process.env.port || DEFAULT_PORT;
+api.get('/contacts/:name', function(req,res,next){
+	res.send();
+})
+
+api.post('/contacts/:name', function(req,res,next){
+	if (req.params.name ==="exist"){
+		return res.status(403).send();
+	}
+});
+
+api.put('/contacts/:name/:new', function(req,res,next){
+	res.send();
+});
+
+
+api.delete('/contacts/:name/:new', function(req,res,next){
+	res.send();
+});
+
+
 console.log('server started on port 3000');
 api.listen(3000);
 
